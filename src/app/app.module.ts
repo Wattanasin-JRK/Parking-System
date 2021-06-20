29482dfd7d47d4c9aa0015cdf4fa99bf0888b091
EllasEIllos <72708259+EllasEIllos@users.noreply.github.com>
2021-06-20 22:00:32 +0700

    Add Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material-module';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -15,7 +16,9 @@ import { MaterialModule } from './material-module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    AngularFireModule.initializeApp(environment.firebase)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAijB7ztSvcXxlbPRpjU1t6q3MXL3S9YnE",
+    authDomain: "carparkingmana.firebaseapp.com",
+    databaseURL: "https://carparkingmana-default-rtdb.firebaseio.com",
+    projectId: "carparkingmana",
+    storageBucket: "carparkingmana.appspot.com",
+    messagingSenderId: "488559117535",
+    appId: "1:488559117535:web:0c62745bac5a9b340ed56a",
+    measurementId: "G-RGZ01EL9N4"
+  }
 };
 
 /*
